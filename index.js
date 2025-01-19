1ce1ae850ae58f03c3d5b98073f9e845d4e975a7
Yuchen Xin <xin.yuchen@hotmail.com>
2025-01-19 08:21:14 -0800

    Socket IO and callback functionality

--- index.js
+++ index.js
@@ -22,21 +22,31 @@ const PORT = parseInt(process.env.PORT) || 5678;
 io.on('connection', (socket) => {
   console.log('A user connected.');
 
-  socket.on('createRoom', ({ roomCode, callback }) => {
-    createRoom(roomCode, socket.id, callback);
-  });
+  socket.on('createRoom', ({ roomCode, callback }) => createRoom(roomCode, socket.id, callback));
 
-  socket.on('joinRoom', ({ roomCode, username, callback }) => {
-    joinPlayer(socket.id, username, roomCode, callback);
-  });
+  socket.on('joinRoom', ({ roomCode, username, callback }) => joinPlayer(socket.id, username, roomCode, callback));
 
-  socket.on('disconnecting', () => {
-    console.log('A user is disconnecting.');
-  });
+  socket.on('submitTopic', ({ topic, callback }) => getPlayerRoom(socket.id).setTopic(socket.id, topic, callback));
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected.');
-  });
+  socket.on('agreeTopic', ({ callback }) => getPlayerRoom(socket.id).agreeTopic(socket.id, callback));
+
+  socket.on('startPrompt', ({ callback }) => getPlayerRoom(socket.id).startPrompt(socket.id, callback));
+
+  socket.on('submitPrompt', ({ prompt, callback }) => getPlayerRoom(socket.id).setPrompt(socket.id, prompt, callback));
+
+  socket.on('submitResponse', ({ response, callback }) => getPlayerRoom(socket.id).setResponse(socket.id, response, callback));
+
+  socket.on('submitFeedback', ({ feedback, callback }) => getPlayerRoom(socket.id).setFeedback(socket.id, feedback, callback));
+
+  socket.on('submitNextQuestion', ({ callback }) => getPlayerRoom(socket.id).setNextQuestion(socket.id, callback));
+
+  socket.on('submitSummary', ({ callback }) => getPlayerRoom(socket.id).setSummary(socket.id, callback));
+
+  socket.on('submitPlayAgain', ({ callback }) => getPlayerRoom(socket.id).setPlayAgain(socket.id, callback));
+
+  socket.on('disconnecting', () => leavePlayer(socket.id));
+
+  socket.on('disconnect', () => console.log('A user disconnected.'));
 });
 
 server.listen(PORT, () => console.log(`Those Who Know Server listening at port ${PORT}.`));
--- room.js
+++ room.js
@@ -18,33 +18,46 @@ class Room {
     this.badFeedbackCount = 0;
   }
 
-  setTopic(socketId, topic) {
-    if (socketId === this.adminSocketId) {
-      this.topic = topic;
-    }
-  }
-
-  agreeTopic(socketId) {
-    if (socketId !== this.adminSocketId) {
-      this.topicAgreed = true;
-    }
-  }
-
-  startPrompt(socketId) {
-    if (socketId === this.adminSocketId && this.topic !== '' && this.topicAgreed) {
-      this.stage = constants.GAME_STAGES.PROMPT;
-      this.roundsPlayed++;
-    }
-  }
-
-  getOtherSocketId(socketId) {
+  getOtherSocketId(socketId) { // private helper function
     return this.players[0] === socketId ? this.players[1] : this.players[0];
   }
 
-  setPrompt(socketId, prompt) {
+  setTopic(socketId, topic, callback) {
+    if (socketId !== this.adminSocketId) {
+      callback({ success: false, message: 'Only the admin can set the topic.' });
+      return;
+    }
+    this.topic = topic;
+    callback({ success: true, message: 'Success!' });
+  }
+
+  agreeTopic(socketId, callback) {
+    if (socketId === this.adminSocketId || this.topic !== '') {
+      callback({ success: false, message: 'Only the non-admin can agree to the topic.' });
+      return;
+    }
+    this.topicAgreed = true;
+    callback({ success: true, message: 'Success!' });
+  }
+
+  startPrompt(socketId, callback) {
+    if (socketId !== this.adminSocketId || !this.topicAgreed) {
+      callback({ success: false, message: 'Only the admin can start the prompt.' });
+      return;
+    }
+    this.stage = constants.GAME_STAGES.PROMPT;
+    this.roundsPlayed++;
+    callback({ success: true, message: 'Success!' });
+  }
+
+  setPrompt(socketId, prompt, callback) {
     const player = getPlayer(socketId);
-    player.addSelfPrompt(prompt);
+    if (player.promptSubmitted) {
+      callback({ success: false, message: 'You have already submitted a prompt.' });
+      return;
+    }
     player.promptSubmitted = true;
+    player.addSelfPrompt(prompt);
     getPlayer(this.getOtherSocketId(socketId)).addOtherPrompt(prompt);
 
     let numSubmitted = 0;
@@ -56,10 +69,17 @@ class Room {
     if (numSubmitted === this.players.length) {
       this.stage = constants.GAME_STAGES.RESPONSE;
     }
+
+    callback({ success: true, message: 'Success!' });
   }
 
-  setResponse(socketId, response) {
-    getPlayer(socketId).responseSubmitted = true;
+  setResponse(socketId, response, callback) {
+    const player = getPlayer(socketId);
+    if (player.responseSubmitted) {
+      callback({ success: false, message: 'You have already submitted a response.' });
+      return;
+    }
+    player.responseSubmitted = true;
     getPlayer(this.getOtherSocketId(socketId)).addResponse(response);
     
     let numSubmitted = 0;
@@ -71,10 +91,17 @@ class Room {
     if (numSubmitted === this.players.length) {
       this.stage = constants.GAME_STAGES.ADVISING; // players give each other feedback
     }
+
+    callback({ success: true, message: 'Success!' });
   }
 
-  setFeedback(socketId, feedback) {
-    getPlayer(socketId).feedbackSubmitted = true;
+  setFeedback(socketId, feedback, callback) {
+    const player = getPlayer(socketId);
+    if (player.feedbackSubmitted) {
+      callback({ success: false, message: 'You have already submitted a feedback.' });
+      return;
+    }
+    player.feedbackSubmitted = true;
     const ret = getPlayer(this.getOtherSocketId(socketId)).addFeedback(feedback);
     this.badFeedbackCount += ret ? 0 : 1;
 
@@ -87,9 +114,11 @@ class Room {
     if (numSubmitted === this.players.length) {
       this.stage = constants.GAME_STAGES.FEEDBACK; // players receive each others' feedback
     }
+
+    callback({ success: true, message: 'Success!' });
   }
 
-  setNextQuestion(socketId) {
+  setNextQuestion(socketId, callback) {
     getPlayer(socketId).nextQuestionSubmitted = true;
 
     let numSubmitted = 0;
@@ -106,19 +135,26 @@ class Room {
       this.roundsPlayed++;
       this.badFeedbackCount = 0;
     }
+
+    callback({ success: true, message: 'Success!' });
   }
 
-  setSummary(socketId) {
-    if (socketId === this.adminSocketId) {
-      // TODO: add LLM summary generation here
-      for (const id of this.players) {
-        getPlayer(id).setSummary('TMP summary here');
-      }
-      this.stage = constants.GAME_STAGES.SUMMARY;
+  setSummary(socketId, callback) {
+    if (socketId !== this.adminSocketId) {
+      callback({ success: false, message: 'Only the admin can set the summary.' });
+      return;
     }
+
+    // TODO: add LLM summary generation here
+    for (const id of this.players) {
+      getPlayer(id).setSummary('TMP summary here');
+    }
+    this.stage = constants.GAME_STAGES.SUMMARY;
+
+    callback({ success: true, message: 'Success!' });
   }
 
-  setPlayAgain(socketId) {
+  setPlayAgain(socketId, callback) {
     getPlayer(socketId).playAgainSubmitted = true;
 
     let numSubmitted = 0;
@@ -137,6 +173,8 @@ class Room {
       }
       this.badFeedbackCount = 0;
     }
+
+    callback({ success: true, message: 'Success!' });
   }
 
   getGameState(socketId) {
@@ -181,6 +219,7 @@ const joinPlayer = (socketId, username, roomCode, callback) => {
     return;
   }
   room.players.push(socketId);
+  callback({ success: true, message: 'Success!' });
 };
 
 const leavePlayer = (socketId) => { // no callback for this
